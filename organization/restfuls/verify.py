8709b05a4dc32cb0e089d03d1567655253c96458
binfreedom <250440083@qq.com>
2014-01-12 08:28:34 +0800

    22222

--- organization/restfuls/verify.py
+++ organization/restfuls/verify.py
@@ -21,8 +21,8 @@ def activity_s():
 
         if MsgType == "text":
             return response_text(xml_recv, web_chat)
-        #elif MsgType =="event":
-        #    return 'event'
+        elif MsgType =="event":
+            return 'event'
 
 def response(web_chat, reply_dict, reply_type):
     reply = web_chat.reply(reply_type, reply_dict)
@@ -43,25 +43,25 @@ def response_text(xml_recv, web_chat):
     ToUserName = xml_recv.find("ToUserName").text
     FromUserName = xml_recv.find("FromUserName").text
 
-    #if content == 'h':
-    #    return HELP
-    #elif content == 'my':
-    #    return 'waiting……'
-    #else:
-    activity_id = string.atoi(content)
-    activity = get_activity_weixin(activity_id)
-    reply_dict = {
-        "ToUserName": FromUserName,
-        "FromUserName": ToUserName,
-        "ArticleCount": 1,
-        "item": [{
-            "Title": str(activity.title),
-            "Description": str(activity.content),
-            "PicUrl": BASE_URL+'/static/image/huodong1.jpg',
-            "Url": url(activity_id)
-        }]
-    }
-    return response(web_chat, reply_dict, "news")
+    if content == 'h':
+        return HELP
+    elif content == 'my':
+        return 'waiting……'
+    else:
+        activity_id = string.atoi(content)
+        activity = get_activity_weixin(activity_id)
+        reply_dict = {
+            "ToUserName": FromUserName,
+            "FromUserName": ToUserName,
+            "ArticleCount": 1,
+            "item": [{
+                "Title": str(activity.title),
+                "Description": str(activity.content),
+                "PicUrl": BASE_URL+'/static/image/huodong1.jpg',
+                "Url": url(activity_id)
+            }]
+        }
+        return response(web_chat, reply_dict, "news")
 
 def response_event(xml_recv, web_chat):
     #Event = xml_recv.find("Event").text
@@ -88,7 +88,7 @@ def response_event(xml_recv, web_chat):
 
 
 BASE_URL = "http://bilibili.kejukeji.com"
-#HELP = "感谢关注客聚科技活动平台，输入'h'获取帮助信息，输入'my'获取我参与的活动，输入'c'创建一个新活动"
+HELP = "感谢关注客聚科技活动平台，输入'h'获取帮助信息，输入'my'获取我参与的活动，输入'c'创建一个新活动!"
 
 def url(activity_id):
     return BASE_URL+"/showactivity/"+str(activity_id)
