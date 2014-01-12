db4b095fbd24bc126aa3abfc153ef3bb9e3d6c7f
binfreedom <250440083@qq.com>
2014-01-12 08:50:44 +0800

    3333

--- organization/restfuls/verify.py
+++ organization/restfuls/verify.py
@@ -76,27 +76,20 @@ def response_text(xml_recv, web_chat):
         return response(web_chat, reply_dict, "news")
 
 def response_event(xml_recv, web_chat):
-    #Event = xml_recv.find("Event").text
-    #EventKey = xml_recv.find("EventKey").text
+    Event = xml_recv.find("Event").text
+    EventKey = xml_recv.find("EventKey").text
     activity_id = string.atoi(xml_recv.find("Content").text)
     ToUserName = xml_recv.find("ToUserName").text
     FromUserName = xml_recv.find("FromUserName").text
 
-    #if (Event == 'CLICK') and (EventKey == 'story'):
-    activity = get_activity_weixin(activity_id)
-    reply_dict = {
-        "ToUserName": FromUserName,
-        "FromUserName": ToUserName,
-        "ArticleCount": 1,
-        "item": [{
-            "Title": str(activity.title),
-            "Description": str(activity.content),
-            "PicUrl": BASE_URL+'/static/image/huodong1.jpg',
-            "Url": url(activity_id)
-        }]
-    }
-
-    return response(web_chat, reply_dict, "news")
+    if Event == 'subscribe' :
+        message = HELP
+        reply_dict = {
+            "ToUserName": FromUserName,
+            "FromUserName": ToUserName,
+            "Content":message
+        }
+        return response(web_chat, reply_dict, "text")
 
 
 BASE_URL = "http://bilibili.kejukeji.com"
